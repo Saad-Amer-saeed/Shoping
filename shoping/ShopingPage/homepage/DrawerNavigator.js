41d5638cb9a5d49d16b5a07d7c3bf8da3ee23662
saad-Alqattan <eng.elect.saadamer@gmail.com>
2024-07-14 11:48:01 +0300

    Fix Bugg

--- shoping/ShopingPage/homepage/DrawerNavigator.js
+++ shoping/ShopingPage/homepage/DrawerNavigator.js
@@ -1,65 +1,8 @@
-// import React from "react";
-// import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
-// import Ionicons from "@expo/vector-icons/Ionicons";
-
-// import { createDrawerNavigator } from "@react-navigation/drawer";
-// import ShopingHomePage from "./HomePage";
-// const Drawer = createDrawerNavigator();
-
-// const DrawerNavigator = () => {
-//   return (
-//     <View style={styles.drawerContainer}>
-//       <Drawer.Navigator>
-//         <Drawer.Screen
-//           name="GemStore"
-//           component={ShopingHomePage}
-//           options={({ navigation }) => ({
-//             title: "المجمعات الطبية",
-//             headerShadowVisible: false,
-
-//             headerRight: () => (
-//               <TouchableOpacity
-//               // onPress={() => navigation.goBack()}
-//               // style={styles.headerButton}
-//               >
-//                 <Ionicons
-//                   name="notifications-outline"
-//                   size={24}
-//                   color="black"
-//                 />
-//               </TouchableOpacity>
-//             ),
-//           })}
-//         />
-//       </Drawer.Navigator>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     backgroundColor: "#ffff",
-//   },
-//   text: {
-//     fontSize: 20,
-//     textAlign: "center",
-//     margin: 10,
-//   },
-//   iconContainer: {
-//     padding: 30, // Added padding
-//   },
-// });
-
-// export default DrawerNavigator;
-
 import React from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import ShopingHomePage from "./HomePage";
+import ShopingHomePage from "./MainPage";
 
 const Drawer = createDrawerNavigator();
 
